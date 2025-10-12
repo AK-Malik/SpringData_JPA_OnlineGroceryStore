88ee85c0cd281ee177f1b67c4c8604b2bfc20918
AK-Malik <akmalik.it@gmail.com>
2025-10-12 01:11:12 -0400

    JPA:Synch-up to Update all the changes by 10.12.2025. Final copy before SK asked to make changes on CRUD operations using hibernate rather nativeQuery

--- src/main/java/com/AK/Jpeproject/SpringData_JPA_OnlineGroceryStore/Oracle1/Repo/GroceryStoreBaseRepo.java
+++
@@ -1,7 +0,0 @@
-package com.AK.Jpeproject.SpringData_JPA_OnlineGroceryStore.Oracle1.Repo;
-
-import com.AK.Jpeproject.SpringData_JPA_OnlineGroceryStore.Oracle1.Entity.OnlineGroceryStore;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroceryStoreBaseRepo extends JpaRepository<OnlineGroceryStore, Long> {
-}
